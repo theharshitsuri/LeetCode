ad5b2c7188605eff90bcad3709c35488ed50682a
Harshit Suri <surih2003@gmail.com>
2024-06-15 23:25:27 -0700

    Create 101.py

---
+++ BInary Tree General/101.py
@@ -0,0 +1,28 @@
+# 101. SYMMETRIC TREE
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        
+        return self.isMirror(root.left, root.right)
+    
+    def isMirror(self, left, right):
+        if not left and not right:
+            return True
+        if not left or not right:
+            return False
+        if left.val != right.val:
+            return False
+        
+        return self.isMirror(left.left, right.right) and self.isMirror(left.right, right.left)
