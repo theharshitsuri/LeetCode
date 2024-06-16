34d08e12e7c109561a23e6f29bfc4f8dbf2a5945
Harshit Suri <surih2003@gmail.com>
2024-06-15 23:21:15 -0700

    Create 104.py
    LeetCode 104

---
+++ BInary Tree General/104.py
@@ -0,0 +1,19 @@
+# 104. MAXIUMUM DEPTH OF BINARY TREE
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root == None:
+            return 0
+        lh = self.maxDepth(root.left)
+        rh = self.maxDepth(root.right)
+        return 1 + max(lh,rh)
