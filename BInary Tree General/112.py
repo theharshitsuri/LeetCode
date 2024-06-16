4b19f68e348c21f8228a9434fc991ddb2cb96960
Harshit Suri <surih2003@gmail.com>
2024-06-15 23:30:17 -0700

    Create 112.py

---
+++ BInary Tree General/112.py
@@ -0,0 +1,26 @@
+# 112. PATH SUM
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right1
+class Solution(object):
+    def hasPathSum(self, root, targetSum):
+        """
+        :type root: TreeNode
+        :type targetSum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+        
+        if not root.left and not root.right:
+            return targetSum == root.val
+        
+        left_sum = self.hasPathSum(root.left, targetSum - root.val)
+        right_sum = self.hasPathSum(root.right, targetSum - root.val)
+        
+        return left_sum or right_sum
+        
