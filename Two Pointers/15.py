5d375aa84ba54cdd333da8c3ffb457378322c4c6
Harshit Suri <surih2003@gmail.com>
2024-06-21 12:48:50 -0700

    Create 15.py

---
+++ Two Pointers/15.py
@@ -0,0 +1,32 @@
+# 15. 3 SUM
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        ans = []
+        nums.sort()
+        l = len(nums)
+        for i in range(l - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            left, right = i + 1, l - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total == 0:
+                    ans.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+        return ans
+
+                
